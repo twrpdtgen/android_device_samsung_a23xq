c9589bb99d60fd5b37d044cfdee5dbfdd5f6474b
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-12-15 03:57:19 +0100

    a23xq: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A236M Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a23xq.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a23xq-user \
+    omni_a23xq-userdebug \
+    omni_a23xq-eng
---
+++ omni_a23xq.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a23xq device
+$(call inherit-product, device/samsung/a23xq/device.mk)
+
+PRODUCT_DEVICE := a23xq
+PRODUCT_NAME := omni_a23xq
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A236M
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a23xqub-user 11 RP1A.200720.012 A236MUBS8EXJ1 release-keys"
+
+BUILD_FINGERPRINT := samsung/a23xqub/a23xq:11/RP1A.200720.012/A236MUBS8EXJ1:user/release-keys
